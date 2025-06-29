60b961a87a4387e871fb7c5eb97dc64333a0bf45
Cong Le <longchike@gmail.com>
2025-06-29 03:52:35 -0700

    The setup

---
+++ Neuron_Communication/Neuron_CommunicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neuron_CommunicationApp.swift
+//  Neuron_Communication
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Neuron_CommunicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
